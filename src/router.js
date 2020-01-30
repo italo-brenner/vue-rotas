6d5baca6edb741e54b1b9fd9270a632edfac2982
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-01-30 14:59:34 -0300

    Capturando parametros das rotas

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './views/Home'
 import Contatos from './views/contatos/Contatos'
+import ContatoDetalhes from './views/contatos/ContatoDetalhes'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,7 @@ export default new VueRouter({
   linkActiveClass: 'active',
   routes: [
     { path: '/contatos', component: Contatos },
+    { path: '/contatos/:id', component: ContatoDetalhes },
     { path: '/', component: Home }
   ]
 })
