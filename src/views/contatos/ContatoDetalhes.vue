06a7085b2d1163eb8152671afdb1a09f7295d836
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-01-30 15:21:02 -0300

    Capturando parametros das rotas

--- src/views/contatos/ContatoDetalhes.vue
+++ src/views/contatos/ContatoDetalhes.vue
@@ -1,3 +1,11 @@
 <template>
-    <h3 class="font-weight light">Detalhes sobre o contrato</h3>
+  <h3 class="font-weight light">Detalhes sobre o contrato com id: {{ $route.params.id }}</h3>
 </template>
+
+<script>
+export default {
+  created() {
+    console.log('Parâmetros da rota: ', this.$route.params)
+  }
+}
+</script>
