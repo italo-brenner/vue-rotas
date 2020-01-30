78a90e883a62f4906156e9f64606fbe18715d3b8
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-01-30 15:55:49 -0300

    Reagindo a alteracoes nos parametros das rotas com beforeRouteUpdate

--- src/views/contatos/ContatoDetalhes.vue
+++ src/views/contatos/ContatoDetalhes.vue
@@ -9,10 +9,9 @@ export default {
       id: this.$route.params.id
     }
   },
-  watch: {
-    '$route'(to, from) {
-      this.id = to.params.id
-    }
+  beforeRouteUpdate(to, from, next) {
+    this.id = to.params.id
+    next()
   },
   created() {
     console.log('Parâmetros da rota: ', this.$route.params)
