77e89419423d83ab63a542a98bb6c36ee333e3c9
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2020-01-30 09:18:25 -0300

    Navegando de forma programatica

--- src/views/contatos/Contatos.vue
+++ src/views/contatos/Contatos.vue
@@ -1,3 +1,16 @@
 <template>
+  <div>
     <h3 class="font-weight-light">Contatos</h3>
+    <button class="btn btn-secondary mt-4 mb-4" @click="voltar">Voltar</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    voltar(event) {
+      this.$router.back()
+    }
+  }
+}
+</script>
